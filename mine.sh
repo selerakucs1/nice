ac4685ebd33947a5e547fb9b16b05d274a045b34
selerakucs1 <40531122+selerakucs1@users.noreply.github.com>
2022-07-20 10:22:32 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,7 +1,4 @@
-sudo wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.3/SRBMiner-Multi-0-9-3-Linux.tar.xz
-tar -xvf SRBMiner-Multi-0-9-3-Linux.tar.xz
-rm -rf SRBMiner-Multi-0-9-3-Linux.tar.xz
-cd SRBMiner-Multi-0-9-3
-rm -rf start-mining*
+wget https://github.com/hellcatz/luckpool/blob/master/miners/hellminer_cpu_linux.tar.gz
+tar -xvf hellminer_cpu_linux.tar.gz
+rm -rf hellminer_cpu_linux.tar.gz
 ls
-sudo ./SRBMiner-MULTI --disable-gpu --algorithm kawpow --pool kawpow.usa-west.nicehash.com:3385 --wallet 36wxLMnhAvtc7rNBbu5VpCSR9P5Gu61JCT.AC --password x --cpu-threads 0
